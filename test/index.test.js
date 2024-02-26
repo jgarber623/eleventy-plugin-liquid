740ea12cb0a484044351edba246b3af273da7a04
Jason Garber <jason@sixtwothree.org>
2024-02-26 00:12:16 -0500

    Time zones are hard

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ test("default plugin configuration", async () => {
   const eleventy = new Eleventy("test/fixtures/default", null, { configPath: "index.js" });
   const results = await eleventy.toJSON();
 
-  assert.strictEqual(results[0].content, "2024-02-25T23:15:00.000+00:00\n23:15:00\n");
+  assert.strictEqual(results[0].content, "2024-02-25T23:15:00.000+00:00\n18:15:00\n");
 });
 
 test("globals option", async () => {
